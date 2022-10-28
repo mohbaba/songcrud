3bdac2e01c71c41c31d4674dfd0d3694e9242190
mohbaba <babamu09@gmail.com>
2022-10-28 20:05:20 +0100

    CRUD Task

--- musicapp/models.py
+++ musicapp/models.py
@@ -6,6 +6,9 @@ class Artiste(models.Model):
     first_name = models.CharField(max_length=100, blank = True)
     last_name = models.CharField(max_length=100, blank = True)
     age = models.IntegerField()
+    
+    def __str__(self):
+        return self.first_name
 
 class Song(models.Model):
     title = models.CharField(max_length=100, blank = True)
@@ -13,6 +16,12 @@ class Song(models.Model):
     likes = models.IntegerField()
     artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
     
+    def __str__(self):
+        return self.title
+    
 class Lyric(models.Model):
     content = models.TextField( blank = True)
     song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.content
