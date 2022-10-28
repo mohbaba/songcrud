3037cd00f991807621aa979a45f95c4f9bb9528e
mohbaba <babamu09@gmail.com>
2022-10-28 19:29:22 +0100

    CRUD Task

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,18 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100, blank = True)
+    last_name = models.CharField(max_length=100, blank = True)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, blank = True)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    
+class Lyric(models.Model):
+    content = models.TextField( blank = True)
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
